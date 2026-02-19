7b071862b8bb1d1de4cba6c154201b8a52e764e4
david <mdavidl@gmail.com>
2026-02-19 12:59:57 +0200

    start to upload to main

--- border-alerts-system/analytics-api/dal.py
+++ border-alerts-system/analytics-api/dal.py
@@ -1,29 +1,115 @@
-from mongo_connection import MongoConnection as mongo,settings
+from redis_connection import Redis
+from mongo_connection import MongoConnection
 
 
-def alerts_by_border_and_priority():
-    mongo_coll = mongo.get_collection(self=)
-    query = mongo_coll.find({},{"border":1,"_id":0})
-    return query
-def top_urgent_zones():
-    mongo_coll = mongo.get_collection(self=)
-    query = mongo_coll.find({},{"zone":1,"_id":0})
-    return query
+def get_border_and_priority_analytics():
+    redis_class = Redis()
+    route_data = redis_class.get_data(route_num='2')
+    if route_data:
+        return route_data
+    else:
+        mongo_class = MongoConnection()
+        collection = mongo_class.get_collection()
+        route_data = collection.aggregate([
+            {
+                "$group": {'border': "$border", 'priority': '$priority', 'count_events': {'$count': '$_id'}}
+            },
+            {
+                "$sort": {"priority": -1, "count_events": -1}
+            }
+        ]).to_list()
+        route = {
+            'data': route_data
+        }
+        redis_class.insert_route_data(route, '2')
+        if route_data:
+            return route
+        else:
+            return None
 
 
-def distance_distribution():
-    mongo_coll = mongo.get_collection(self=)
-    query = mongo_coll.find({})
-    return query
+def get_5_top_urgent_zones():
+    redis_class = Redis()
+    route_data = redis_class.get_data(route_num='1')
+    if route_data:
+        return route_data
+    else:
+        mongo_class = MongoConnection()
+        collection = mongo_class.get_collection()
+        route_data = collection.aggregate([
+            {
+                "$match": {'$priority': 'URGENT'}
+            },
+            {
+                "$group": {'border': "$border", 'count_events': {'$count': '$_id'}}
+            },
+            {
+                "$sort": {"count_events": -1}
+            },
+            {
+                "$limit": 5
+            }
+        ]).to_list()
+        route = {
+            "data": route_data
+        }
+        redis_class.insert_route_data(route, '1')
+        if route_data:
+            return route
+        else:
+            return None
 
 
-def low_visibility_high_activity():
-    mongo_coll = mongo.get_collection(self=)
-    query = mongo_coll.find({},{"border":1,"_id":0})
-    return query
+def get_distance_distribution_analytics():
+    redis_class = Redis()
+    route_data = redis_class.get_data(route_num='3')
+    if route_data:
+        return route_data
+    else:
+        mongo_class = MongoConnection()
+        collection = mongo_class.get_collection()
+        route_data = collection.aggregate([
+            {
+                "$addFields": {
+                    '1-300': {'$distance_from_fence_m': {"$le": 300, "$ge": 0}},
+                    '301-800': {'$distance_from_fence_m': {"$le": 800, "$ge": 301}},
+                    '801-1500': {'$distance_from_fence_m': {"$le": 1500, "$ge": 801}}
+                }
+            },
+            {
+                "$project": {'1-300': "$1-300", '301-800': "$301-800", '801-1500': "$801-1500",
+                             'count_events': {'$count': '$_id'}}
+            },
+        ]).to_list()
+        route = {
+            "data": route_data
+        }
+        redis_class.insert_route_data(route, '3')
+        if route_data:
+            return route
+        else:
+            return None
 
 
-def hot_zones():
-    mongo_coll = mongo.get_collection(self=)
-    query = mongo_coll.find({},{"border":1,"_id":0})
-    return query
+def get_low_visibility_high_activity_analytics():
+    redis_class = Redis()
+    route_data = redis_class.get_data(route_num='3')
+    if route_data:
+        return route_data
+    else:
+        mongo_class = MongoConnection()
+        collection = mongo_class.get_collection()
+        route_data = collection.aggregate([])
+    return None
+
+
+def get_hot_zones_analytics():
+    redis_class = Redis()
+    route_data = redis_class.get_data(route_num='3')
+    if route_data:
+        return route_data
+    else:
+        mongo_class = MongoConnection()
+        collection = mongo_class.get_collection()
+        route_data = collection.aggregate([])
+    return None
--- border-alerts-system/analytics-api/redis_connection.py
+++ border-alerts-system/analytics-api/redis_connection.py
@@ -1,5 +1,22 @@
-from redis import Redis
+import os
+import redis
+import json
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", "6379"))
+REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 
 
-def get_redis_connection():
-    return Redis(host='localhost', port=6379, decode_responses=True)
+class Redis:
+    def get_redis_config(self):
+        return redis.Redis(host='localhost', port=6379, decode_responses=True,db=0)
+    def get_data(self, route_num: str):
+        route_data = self.get_redis_config().get(f"route:{route_num}")
+        if route_data:
+            return json.loads(route_data)  # type: ignore
+        else:
+            return None
+
+    def insert_route_data(self, route_data, route_num):
+        client = self.get_redis_config()
+        client.setex(f"route:{route_num}", 3600, json.dumps(route_data))
