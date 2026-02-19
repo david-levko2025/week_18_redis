f207888c4397e9558abef16769632c8120cdf853
david <mdavidl@gmail.com>
2026-02-19 12:52:27 +0200

    start to upload to main

--- border-alerts-system/analytics-api/redis_connection.py
+++ border-alerts-system/analytics-api/redis_connection.py
@@ -2,4 +2,4 @@ from redis import Redis
 
 
 def get_redis_connection():
-    return Redis(host=settings.REDIS_HOST, port=6379, decode_responses=True)
+    return Redis(host='localhost', port=6379, decode_responses=True)
