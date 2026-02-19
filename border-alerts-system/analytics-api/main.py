1b34ce7da05620ab21ea97731ba3e55e45bfb855
david <mdavidl@gmail.com>
2026-02-19 10:45:49 +0200

    build the main for api

---
+++ border-alerts-system/analytics-api/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+from routes import route as order_route
+
+app = FastAPI(debug=True)
+app.include_router(order_route)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="localhost",port=8000,reload=True)
